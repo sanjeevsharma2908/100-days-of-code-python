1d3140165cc2b3de2db912c1ea5fafd2a889d00d
Sanjeev Sharma <ssharma2418@outlook.com>
2023-12-26 21:48:37 +0530

    The Hangman _ game

--- functions/first.py
+++ functions/first.py
@@ -1,4 +1,9 @@
-def my_function(name):
-    print("Hello World",name)
-    
-my_function("John")
+
+name = 'sanjeev Sharma'
+age = 25
+city = "Delhi"
+
+if(name =='sanjeev Sharma' and age == 25 and city =='Delhi'):
+    print("Hello Sanjeev, How are you today?")
+else:
+    print('Good Bye Beta!')
---
+++ functions/while_1.py
@@ -0,0 +1,2 @@
+# Introduction to the while loop
+# You are given a  number you need to write a program that will be telling that a number is Palindrome or not.

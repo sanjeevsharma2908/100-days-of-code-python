35108ab3bfcf464e0e82f0b4dea0330d9ed43de4
Sanjeev Sharma <69752064+sanjeevsharma2908@users.noreply.github.com>
2025-04-16 22:32:45 +0530

    virtual coffee machine project initiated

---
+++ Digital_Coffee_vending/main.py
@@ -0,0 +1,4 @@
+#TODO 1. Check resources sufficient to make the drink
+
+from menu import MENU
+print(MENU)
---
+++ Digital_Coffee_vending/menu.py
@@ -0,0 +1,27 @@
+MENU = {
+    "espresso":{
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+            
+        },
+        "cost": 1.5,
+    },
+    "latte":{
+         "ingredients": {
+            "water": 200,
+            "coffee": 24,
+            "milk": 150,
+            
+        },
+        "cost": 2.5,
+    },
+    "cappuccino":{
+        "ingredients": {
+            "water": 250,
+            "coffee": 24,
+            "milk": 100,
+        },
+        "cost": 3.0,
+    }
+}

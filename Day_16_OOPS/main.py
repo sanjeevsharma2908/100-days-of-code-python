7f671e11d517daa2c4ccb6094e29cf0c108e53ba
Sanjeev Sharma <69752064+sanjeevsharma2908@users.noreply.github.com>
2025-04-21 09:10:49 +0530

    the first object oriented programming commit

---
+++ Day_16_OOPS/main.py
@@ -0,0 +1,20 @@
+
+from turtle import Turtle,Screen # import the class Turtle from the module turtle
+
+timmy  = Turtle()  # its a class
+
+print(timmy) 
+timmy.shape('turtle')
+timmy.color('red')
+
+timmy.forward(100)
+
+
+
+
+
+# <turtle.Turtle object at 0x000001A2D7E4F5B0>  # this is an object of the class Turtle
+my_screen = Screen()
+print(my_screen.canvheight) # 600  # this is an object of the class Screen
+my_screen.exitonclick() # this will close the screen when you click on it
+# this is a simple program to demonstrate the use of classes and objects in python

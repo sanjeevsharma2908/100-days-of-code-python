2ec1aaab24ce6b68268b33f4516bed8e14ede926
Sanjeev Sharma <69752064+sanjeevsharma2908@users.noreply.github.com>
2025-05-06 12:54:39 +0530

    Add scoreboard and enhance ball mechanics for Pong game
    Introduce a scoreboard to track points for both players. Refine ball mechanics by adding directional bounces, adjustable speed, and position reset upon scoring. Update main game loop to handle scoring logic and improve collisions.

--- PythonProject/pong-game/ball.py
+++ PythonProject/pong-game/ball.py
@@ -1,4 +1,6 @@
+import turtle
 from turtle import Turtle
+turtle.speed('fastest')
 
 
 class Ball(Turtle):
@@ -9,11 +11,22 @@ class Ball(Turtle):
         self.penup()
         self.x_move  = 10
         self.y_move = 10
+        self.move_speed = 0.1
 
     def move(self):
-        new_x =self.xcor() + 10
-        new_y =self.ycor() + 10
+        new_x =self.xcor() + self.x_move
+        new_y =self.ycor() + self.y_move
         self.goto(new_x, new_y)
 
-    def bounce(self):
+
+    def bounce_y(self):
         self.y_move *= -1
+
+    def bounce_x(self):
+        self.x_move *= -1
+        self.move_speed *= 0.0000009
+
+    def reset_position(self):
+        self.goto(0,0)
+        self.move_speed  = 0.1
+        self.bounce_x()
--- PythonProject/pong-game/main.py
+++ PythonProject/pong-game/main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 import time
 from ball import Ball
 from paddle import Paddle
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.bgcolor('black')
@@ -9,8 +10,10 @@ screen.setup(width=800, height=600)
 screen.title('Pong')
 screen.tracer(0)
 
+
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+scoreboard = Scoreboard()
 
 ball = Ball()
 
@@ -24,15 +27,27 @@ screen.onkey(l_paddle.go_down, 's')
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
     #Detecting the collision with the wall
-    if ball.ycor() > 280 or ball.xcor() < -280:
-        ball.bounce()
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
 
+    #Detect Collision with the right paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
+    #Detect R paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    # Detect L paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
 
 
 

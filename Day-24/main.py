0b90ae963eed46a9f005281fbe539f6bef8abd2a
Sanjeev Sharma <69752064+sanjeevsharma2908@users.noreply.github.com>
2025-05-10 14:59:06 +0530

    Update scoreboard with high score persistence and file handling
    Added functionality to save and retrieve the high score from a file in the `Scoreboard` class. Adjusted file handling logic in `main.py` to append data, and created a new `data.txt` file for storing the high score.

--- Day-24/main.py
+++ Day-24/main.py
@@ -4,7 +4,8 @@ print(content)
 file.close()
 #With the above method we have to close the file manually after reading the content
 
-with open("my_file.txt") as file:
-    content = file.read()
-    print(content)
+with open("my_file.txt",'a') as file:
+    file.write("currently I'am Learning Python")
+
  # But By using this one we are no longer required to close the file
+                                 
